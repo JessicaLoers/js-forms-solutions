4b6593b283687a7256340bcefa0fc9c9a0bdd2eb
Jessica Lörs <jessica.loers@gmail.com>
2024-03-06 12:32:39 +0100

    add checkinput solution

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -1,24 +1,49 @@
-console.clear();
-
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
 
-function hideTosError() {
-  tosError.setAttribute("hidden", "");
+// --v-- Bonus: Success message! --v--
+const successMessageElemeent = document.querySelector('[data-js="success"]');
+
+function hideSuccess() {
+  successMessageElemeent.setAttribute('hidden', '');
 }
+
+function showSuccess() {
+  successMessageElemeent.removeAttribute('hidden');
+}
+// --^-- Bonus: Success message! --^--
+
+function hideTosError() {
+  tosError.setAttribute('hidden', '');
+}
+
+hideTosError();
+hideSuccess();
 
 function showTosError() {
-  tosError.removeAttribute("hidden");
+  tosError.removeAttribute('hidden');
 }
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
 
+  const form = new FormData(event.target);
+  const fromData = Object.fromEntries(form);
+
+  if (fromData.tos) {
+    hideTosError();
+    showSuccess();
+  } else {
+    showTosError();
+    hideSuccess();
+    return;
+  }
+
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  alert('Form submitted');
 });
