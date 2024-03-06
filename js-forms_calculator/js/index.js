609ec2c75fba832277256e1ecc5eb1c098ee9c7f
Jessica Lörs <jessica.loers@gmail.com>
2024-03-06 12:40:44 +0100

    add calculator solution

--- js-forms_calculator/js/index.js
+++ js-forms_calculator/js/index.js
@@ -1,5 +1,3 @@
-console.clear();
-
 const form = document.querySelector('[data-js="form"]');
 const resultOutput = document.querySelector('[data-js="result"]');
 
@@ -19,12 +17,30 @@ function divide(a, b) {
   return a / b;
 }
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   let result;
 
   // --v-- write your code here --v--
+
+  const form = new FormData(event.target);
+  const formData = Object.fromEntries(form);
+
+  const numberA = parseInt(formData.numberA, 10);
+  const numberB = parseInt(formData.numberB, 10);
+
+  if (formData.operator === 'addition') {
+    result = add(numberA, numberB);
+  } else if (data.operator === 'subtraction') {
+    result = subtract(numberA, numberB);
+  } else if (formData.operator === 'multiplication') {
+    result = multiply(numberA, numberB);
+  } else if (formData.operator === 'division') {
+    result = divide(numberA, numberB);
+  } else {
+    result = null;
+  }
 
   // --^-- write your code here --^--
 
