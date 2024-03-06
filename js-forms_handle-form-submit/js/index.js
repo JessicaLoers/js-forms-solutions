06235cb958618e3d16c989ef77a19513f2428f30
Jessica Lörs <jessica.loers@gmail.com>
2024-03-06 12:16:54 +0100

    add solution handle from submit

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,13 @@
-console.clear();
-
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const from = new FormData(event.target);
+  const fromData = Object.fromEntries(from);
+
+  console.log('object of all input elements ', fromData);
+
+  // The badness bonus
+  console.log(`age-badness-sum is ${parseInt(fromData.badness, 10) + parseInt(fromData.age, 10)}`);
+});
